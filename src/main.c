30ef89b97e33553ae7068879c89312b77fc3e2e2
Daniel Lovasko <daniel.lovasko@gmail.com>
2017-05-07 11:36:05 +0100

    adjusted scene settings

--- src/main.c
+++ src/main.c
@@ -17,8 +17,8 @@
 static persp cam;
 
 /* Scene objects. */
-static object objs[2];
-static sphere sp;
+static object objs[4];
+static sphere sps[3];
 static plane pl;
 
 static void
@@ -58,19 +58,33 @@ init_camera(void)
 static void
 init_scene(void)
 {
-  /* vector_init(&sp.sp_pos, 0.0f, 1.0f, 2.0f); */
-  /* sp.sp_rad = 1.0f; */
-  /* objs[0].ob_geo = &sp; */
-  /* objs[0].ob_mat = NULL; */
-  /* objs[0].ob_gt  = OBJ_SPHERE; */
-  /* objs[0].ob_mt  = 0; */
+  vector_init(&sps[0].sp_pos, 0.0f, 2.0f, 5.0f);
+  sps[0].sp_rad = 2.0f;
+  objs[0].ob_geo = &sps[0];
+  objs[0].ob_mat = NULL;
+  objs[0].ob_gt  = OBJ_SPHERE;
+  objs[0].ob_mt  = 0;
+
+  vector_init(&sps[1].sp_pos, 1.0f, 1.0f, 3.0f);
+  sps[1].sp_rad = 1.0f;
+  objs[1].ob_geo = &sps[1];
+  objs[1].ob_mat = NULL;
+  objs[1].ob_gt  = OBJ_SPHERE;
+  objs[1].ob_mt  = 0;
+
+  vector_init(&sps[2].sp_pos, -2.0f, 0.5f, 6.0f);
+  sps[2].sp_rad = 0.5f;
+  objs[2].ob_geo = &sps[2];
+  objs[2].ob_mat = NULL;
+  objs[2].ob_gt  = OBJ_SPHERE;
+  objs[2].ob_mt  = 0;
 
   vector_init(&pl.pl_pos, 0.0f, 0.0f, 0.0f);
   vector_init(&pl.pl_nor, 0.0f, 1.0f, 0.0f);
-  objs[0].ob_geo = &pl;
-  objs[0].ob_mat = NULL;
-  objs[0].ob_gt  = OBJ_PLANE;
-  objs[0].ob_mt  = 0;
+  objs[3].ob_geo = &pl;
+  objs[3].ob_mat = NULL;
+  objs[3].ob_gt  = OBJ_PLANE;
+  objs[3].ob_mt  = 0;
 }
 
 int
@@ -97,7 +111,7 @@ main(void)
   for (y = 0; y < 500; y++) {
     for (x = 0; x < 500; x++) {
       cam_persp_primary(&cam, coord(x, 500), coord(y, 500), &r);
-      scene_array_intersect(objs, 1, &r, &i);
+      scene_array_intersect(objs, 4, &r, &i);
       proc_debug_compute(&r, &i, &c);
       fprintf(output, "%u %u %u ",
              scale(c.vc_x, 255),
