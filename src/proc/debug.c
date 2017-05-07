c6c1c13324653aef25c32e84c35e09c51da3b93d
Daniel Lovasko <daniel.lovasko@gmail.com>
2017-05-07 11:38:20 +0100

    removed vector norm from the debug procedure

--- src/proc/debug.c
+++ src/proc/debug.c
@@ -1,19 +1,17 @@
 #include <math.h>
 #include <float.h>
 
-#include "proc/debug.h"
 #include "geom/isect.h"
+#include "proc/debug.h"
 
 void
 proc_debug_compute(ray* r, isect* i, vector* c)
 {
-  float angle;
+  float x;
 
   if (i->is_t < FLT_MAX) {
-    vector_norm(&r->ry_dir);
-    vector_norm(&i->is_nor);
-    vector_dot(&r->ry_dir, &i->is_nor, &angle);
-    vector_repeat(c, fabsf(angle));
+    vector_dot(&r->ry_dir, &i->is_nor, &x);
+    vector_repeat(c, fabsf(x));
   } else {
     vector_repeat(c, 0.0f);
   }
